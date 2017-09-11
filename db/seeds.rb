79760c761fa92c4a348fe0476b2c7f2f2460bb5c
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-09-11 18:37:27 +0200

    generated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+p "Start destroying"
+
+User.destroy_all
+Product.destroy_all
+Tag.destroy_all
+
+p "Finish destroying"
+p "_________________"
+
+i = 0
+
+10.times do
+  i += 1
+  p "Generating user##{i}"
+  user = User.create(email: Faker::Internet.email(Faker::RickAndMorty.character), password: "azerty")
+  10.times do
+    product = Product.new
+    product.title = Faker::Commerce.product_name
+    product.price = rand(1000)
+    product.description = Faker::Hipster.sentence
+    product.user = user
+    product.save
+  end
+end
